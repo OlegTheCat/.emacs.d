414e53381ad9bdea3084c2abb9e70a551e817a7e
Oleh Palianytsia <oleh.palianytsia@gmail.com>
2015-10-20 22:02:47 +0300

    Add more keybindings for god-mode

--- inits/init-god-mode.el
+++ inits/init-god-mode.el
@@ -1,5 +1,7 @@
 (require 'god-mode)
 
+(god-mode)
+
 (global-set-key (kbd "<escape>") 'god-local-mode)
 
 (defun update-cursor-for-god-mode ()
@@ -24,5 +26,10 @@
 
 (define-key god-local-mode-map (kbd "i") 'god-local-mode)
 (define-key god-local-mode-map (kbd ".") 'repeat)
+
+(global-set-key (kbd "C-x C-1") 'delete-other-windows)
+(global-set-key (kbd "C-x C-2") 'split-window-below)
+(global-set-key (kbd "C-x C-3") 'split-window-right)
+(global-set-key (kbd "C-x C-0") 'delete-window)
 
 (provide 'init-god-mode)
