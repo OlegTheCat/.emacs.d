63ae4691bd612aac5641140199960d271095ece1
Oleh Palyanytsya <oleh.palianytsia@gmail.com>
2015-11-04 22:11:54 +0200

    Add more setup for ido

--- inits/init-ido.el
+++ inits/init-ido.el
@@ -1,5 +1,7 @@
 (require 'ido)
-(ido-mode t)
+
+(ido-mode 1)
+(ido-everywhere 1)
 
 (setq ido-enable-prefix nil
       ido-enable-flex-matching t
@@ -12,5 +14,19 @@
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+(require-package 'ido-ubiquitous)
+(ido-ubiquitous-mode 1)
+
+(require-package 'ido-ubiquitous)
+(ido-ubiquitous-mode 1)
+
+(require-package 'ido-vertical-mode)
+(ido-vertical-mode 1)
+(setq ido-vertical-define-keys 'C-n-and-C-p-only)
+
+(require-package 'flx-ido)
+(flx-ido-mode 1)
+(setq ido-use-faces nil)
 
 (provide 'init-ido)
