994d169559e2931f0b562c93ecb4d71b9f6d403b
Oleh Palianytsia <oleh.palianytsia@gmail.com>
2015-12-01 22:42:17 +0200

    Disable company auto popup

--- inits/init-company.el
+++ inits/init-company.el
@@ -3,6 +3,8 @@
 
 (global-set-key (kbd "C-x C-o") 'company-complete)
 
+(setq company-idle-delay nil)
+
 (setq company-tooltip-limit 20)
 (setq company-tooltip-align-annotations t)
 (setq-default company-quickhelp-delay nil)
