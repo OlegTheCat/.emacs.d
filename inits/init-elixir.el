d16e330133a5765c7f76865b290923580211ace2
Oleh Palianytsia <oleh.palianytsia@gmail.com>
2015-12-03 00:12:32 +0200

    Extend elixir setup

--- inits/init-elixir.el
+++ inits/init-elixir.el
@@ -1,4 +1,11 @@
 (require-package 'elixir-mode)
 (require-package 'alchemist)
 
+(add-hook 'alchemist-mode-hook 'company-mode)
+(add-hook 'alchemist-iex-mode-hook 'company-mode)
+(add-hook 'elixir-mode-hook
+	  'electric-pair-mode)
+
+(setq alchemist-test-status-modeline nil)
+
 (provide 'init-elixir)
