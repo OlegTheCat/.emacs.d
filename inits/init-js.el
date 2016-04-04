1088458ec1008878903940ef137f763eb5c86862
Oleh Palianytsia <oleh.palianytsia@gmail.com>
2016-04-04 20:53:07 +0300

    Use web-mode for js

--- inits/init-js.el
+++ inits/init-js.el
@@ -1,7 +1,17 @@
 (provide 'init-js)
 
+(require-package 'json-mode)
 (require-package 'js2-mode)
-(add-hook 'js-mode-hook 'js2-minor-mode)
+(require-package 'web-mode)
+
+(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
+
+(setq web-mode-content-types-alist
+  '(("jsx" . "\\.js[x]?\\'")))
+
+(setq web-mode-markup-indent-offset 2)
+(setq web-mode-css-indent-offset 2)
+(setq web-mode-code-indent-offset 2)
 
 (define-key js-mode-map "{" 'paredit-open-curly)
 (define-key js-mode-map "}" 'paredit-close-curly-and-newline)
