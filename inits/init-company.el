eb3bb97e96306b97a6efbcca99b0b8504745c1ee
Oleh Palianytsia <oleh.palianytsia@gmail.com>
2016-08-12 00:00:21 +0300

    Select candidates with tab key

--- inits/init-company.el
+++ inits/init-company.el
@@ -1,18 +1,25 @@
 (require-package 'company)
 (require-package 'company-quickhelp)
 
-(global-set-key (kbd "M-SPC") 'company-complete)
-
 (defun indent-or-complete ()
   (interactive)
   (if (looking-at "\\_>")
       (company-complete-common)
     (indent-according-to-mode)))
 
+(global-set-key (kbd "M-SPC") 'company-complete)
 (global-set-key (kbd "TAB") 'indent-or-complete)
 
-(setq company-idle-delay nil)
+(define-key company-active-map [tab]
+  '(lambda ()
+     (interactive)
+     (company-complete-common-or-cycle 1)))
+(define-key company-active-map [backtab]
+  '(lambda ()
+     (interactive)
+     (company-complete-common-or-cycle -1)))
 
+(setq company-idle-delay nil)
 (setq company-tooltip-limit 20)
 (setq company-tooltip-align-annotations t)
 (setq-default company-quickhelp-delay nil)
